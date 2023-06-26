4953f186af5550f1ef9617638d2f9e9e0c4410d8
Bryan <bryanflores12345@gmail.com>
2023-06-26 12:38:33 -0400

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: ['airbnb', 'airbnb-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="w-screen h-screen overflow-auto bg-gray-200">
+      <div className="w-[60%] h-full mx-auto bg-white shadow-lg px-4 py-6"></div>
+    </div>
+  )
+}
+export default App
